37910d8f499cb1ca4b51024b92c89fe59f27e400
Ivan <isolis@solavcr.com>
2021-09-04 09:14:41 -0600

    [ADD] Basic backend structure

---
+++ src/test/java/bases1/bases/BasesApplicationTests.java
@@ -0,0 +1,13 @@
+package bases1.bases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
